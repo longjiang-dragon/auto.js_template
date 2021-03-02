ccb7f05bf0ff65ccaa17bbf21276c4869457a67f
jianglong <jianglong@sune365.com.cn>
2021-03-02 18:27:12 +0800

    [add]长按

--- weixin.js
+++ weixin.js
@@ -13,9 +13,8 @@ id('b4r').click();
 sleep(1000)
 
 // 查找最后一条消息,并长按
-let children=id("ala").findOne().children();
-children[children.length-2]
-.findOne(className("android.widget.LinearLayout").depth(13)).findOne(className('android.view.View')).longClick();
+let children=id("an3").findOne().children();
+children[children.length-1].findOne(id("ala")).longClick();
 
 
 
