3632465df1ccd77fecb74fe099214d12c874d360
jianglong <jianglong@sune365.com.cn>
2021-03-04 11:01:14 +0800

    [add]添加确认弹窗

--- weixin.js
+++ weixin.js
@@ -9,7 +9,6 @@ auto.setWindowFilter(function (window) {
 //滚动
 //className ("ListView") .scrollForward()
 
-console.log('-------------开始执行------------------')
 let isEnd = false
 //当前第几页
 let pageNum = 0
@@ -19,71 +18,75 @@ const IS_END_TAG = 'IS_END_TAG'
 //下一次选中的item
 let nextBatchItemText = null
 //任务开始时间
-let startTimestamp;
+let startTimestamp
 //任务结束时间
-let endTimestamp;
+let endTimestamp
 
+let confirmStart = confirm('确认开始批量任务?')
 
-id('en5').findOne().parent().parent().click()
-sleep(1000)
-
-while (!isEnd) {
-  //startTimestamp=new Data();
-  //log(dateFtt('yyyy-MM-dd hh:mm:ss', new Date()))
-  // 查找最后一条消息,并长按
-  let children = id('euo').find()
-  children[children.length - 1].longClick()
-
-  // 点击发送给朋友
-  sleep(1000)
-  click('转发', 0)
-
-  //切换为多选
-  sleep(1000)
-  click(936 + 20, 75 + 20)
+if (confirmStart) {
+  console.log('-------------开始执行------------------')
+  id('en5').findOne().parent().parent().click()
   sleep(1000)
 
-  for (let i = 0; i < pageNum * SELECT_COUNT; i++) {
-    //每次滚动1个item
-    swipe(200, 2000, 200, 2000 - (i === 0 ? 450 : 195), 80)
-  }
+  while (!isEnd) {
+    //startTimestamp=new Data();
+    //log(dateFtt('yyyy-MM-dd hh:mm:ss', new Date()))
+    // 查找最后一条消息,并长按
+    let children = id('euo').find()
+    children[children.length - 1].longClick()
+
+    // 点击发送给朋友
+    sleep(1000)
+    click('转发', 0)
+
+    //切换为多选
+    sleep(1000)
+    click(936 + 20, 75 + 20)
+    sleep(1000)
+
+    for (let i = 0; i < pageNum * SELECT_COUNT; i++) {
+      //每次滚动1个item
+      swipe(200, 2000, 200, 2000 - (i === 0 ? 450 : 195), 80)
+    }
 
 //选中需要发送的用户
-  let forwardChildren = id('emw').find()
+    let forwardChildren = id('emw').find()
 
-  //判断是否已到最后一个item
-  isEnd = judgeEndItem(forwardChildren, nextBatchItemText)
-  if (isEnd) continue
+    //判断是否已到最后一个item
+    isEnd = judgeEndItem(forwardChildren, nextBatchItemText)
+    if (isEnd) continue
 
-  let reverseArr = generateReverseArr(forwardChildren, nextBatchItemText)
-  for (let i = 0; i < reverseArr.length; i++) {
-    let child = reverseArr[i]
-    let itemText = child.child(0).getText().toString()
-    let bounds = child.bounds()
-    click(bounds.centerX(), bounds.centerY())
-    sleep(10)
+    let reverseArr = generateReverseArr(forwardChildren, nextBatchItemText)
+    for (let i = 0; i < reverseArr.length; i++) {
+      let child = reverseArr[i]
+      let itemText = child.child(0).getText().toString()
+      let bounds = child.bounds()
+      click(bounds.centerX(), bounds.centerY())
+      sleep(10)
+    }
+    //更新下一次开始的item
+    nextBatchItemText = getNextBatchItemText(forwardChildren)
+    console.log('下一次批量开始的群组名=====', nextBatchItemText)
+
+    //发送群发消息
+    textStartsWith('确定').findOne().click()
+    sleep(500)
+    id('bpc').findOne().click()
+    //log(dateFtt('yyyy-MM-dd hh:mm:ss', new Date()))
+
+    pageNum++
+    back()
+    sleep(500)
   }
-  //更新下一次开始的item
-  nextBatchItemText = getNextBatchItemText(forwardChildren)
-  console.log('下一次批量开始的群组名=====', nextBatchItemText)
 
-  //发送群发消息
-  textStartsWith('确定').findOne().click()
-  sleep(500)
-  id('bpc').findOne().click()
-  //log(dateFtt('yyyy-MM-dd hh:mm:ss', new Date()))
-
-  pageNum++
   back()
   sleep(500)
+  back()
+
+  console.log('-------------执行完成------------------')
+
 }
-
-back()
-sleep(500)
-back()
-
-
-console.log('-------------执行完成------------------')
 
 /**
  * 获取下一次批量发送时，需要找的第一个元素
@@ -143,8 +146,6 @@ function findStartIndex (children, startBatchItemText) {
   console.log('result======', result, startBatchItemText)
   return result === -1 ? 0 : result
 }
-
-console.log('-----------end------------')
 
 //console.log("bbb===",text('霜').findOne().click());
 
