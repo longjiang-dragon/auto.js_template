adf7798df5597ff6c10402f97b87dc5b772fc8ec
jianglong <jianglong@sune365.com.cn>
2021-03-13 09:29:02 +0800

    [add]1499完善

--- wm_1499.js
+++ wm_1499.js
@@ -2,43 +2,67 @@
 // log(titleView);
 // let  priceView=text("1499.00").findOne();
 
+console.show()
+let currentDate = new Date()
+console.warn(
+  '抢购时间：' + currentDate.getFullYear() + '年' + currentDate.getMonth() + '月' +
+  currentDate.getDate() + '日9点')
+const CLICK_DELAY_TIME = 200
 
+let lastTime = new Date().getTime()
+let startTime = getStartTime()
+judgeIsStart()
 
-console.show();
-let currentDate=new Date();
-console.warn("抢购时间："+currentDate.getFullYear()+"年"+currentDate.getMonth()+"月"+ currentDate.getDate()+"日9点")
-
-let  priceView=text("立即抢购").find()[1];
-
-
-while(!priceView){
-     sleep(1000);
-    priceView=text("立即抢购").find()[1]
-    log("查找入口中....")
-   
+function judgeIsStart () {
+  setTimeout(() => {
+    if (new Date().getTime() > startTime) {
+      startTask()
+    } else {
+      judgeIsStart()
+    }
+  }, CLICK_DELAY_TIME)
 }
 
-console.error("已找到入口,等待抢购!!!!")
+function startTask () {
+  click('立即抢购', 2)
+  text('预约抢购').findOne(100000).click()
+  text('确认下单').findOne(100000).click()
+}
 
-let endTime =new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 9).getTime();
+function getStartTime () {
+  let current = new Date()
+  return new Date(current.getFullYear(), current.getMonth(), current.getDate(),
+    9, 59, 59, 500).getTime()
+}
 
-
-
-let interval=setInterval(() => {
-    if(new Date().getTime()+700 > endTime){
-        priceView.click();
-        clearInterval(interval)
-    }
-}, 50);
-
-
-//自动点击抢购
-setTimeout(() => {
-    let  goBuy=text("立即抢购").untilFind();  
-    if(goBuy){
-        goBuy.click();
-    }  else{
-        log("未找到入口....")
-    }
-}, 100);
+// while(!priceView){
+//      sleep(1000);
+//     priceView=text("立即抢购").find()[1]
+//     log("查找入口中....")
+//
+// }
+//
+// console.error("已找到入口,等待抢购!!!!")
+//
+// let endTime =new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 9).getTime();
+//
+//
+//
+// let interval=setInterval(() => {
+//     if(new Date().getTime()+700 > endTime){
+//         priceView.click();
+//         clearInterval(interval)
+//     }
+// }, 50);
+//
+//
+// //自动点击抢购
+// setTimeout(() => {
+//     let  goBuy=text("立即抢购").untilFind();
+//     if(goBuy){
+//         goBuy.click();
+//     }  else{
+//         log("未找到入口....")
+//     }
+// }, 100);
 
