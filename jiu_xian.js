8a4ee06d1166ad2c956d71a96dd32f725678df0e
jianglong <jianglong@sune365.com.cn>
2021-03-25 10:14:13 +0800

    [add]完善酒仙代码

--- jiu_xian.js
+++ jiu_xian.js
@@ -34,3 +34,24 @@ function getStartTime () {
   let current = new Date()
   return new Date(current.getFullYear(), current.getMonth(), current.getDate(),9, 59, 59, 100).getTime()
 }
+
+judgeIsStart()
+
+function judgeIsStart () {
+  setTimeout(() => {
+    if (new Date().getTime() > startTime) {
+      startTask()
+    } else {
+      judgeIsStart()
+    }
+  }, CLICK_DELAY_TIME)
+}
+
+
+function startTask(){
+  text('立即购买').findOne(100000).click()
+  text('我已阅读并同意以上内容').findOne(100000).click()
+  text('确认').findOne(100000).click()
+  text('提交订单').findOne(100000).click()
+}
+
